b6609db0f922af768e059d79b0e1ed5087366709
lee <lee@leedeMac-mini.local>
2023-12-15 14:10:39 +0800

    Your commit message

--- query_solver.py
+++ query_solver.py
@@ -40,28 +40,65 @@ def run_query(programming_problem, api_key):
     # Create user proxy agent, coder, product manager
     user_proxy = autogen.UserProxyAgent(
         name="User_proxy",
-        system_message="A human admin who will give the idea and run the code provided by Coder.",
+        system_message="You are the demand side for market size research results, assigning corresponding tasks to different roles [Market_research_agent, Industry_analysis_agent, Competition_analysis_agent,Customer_needs_analysis_agent,Macroeconomic_analysis_agent,Technology_analysis_agent,Data_analysis_agent,Market_size_forecasting_agent,Market_size_simulation_agent,Market_size_reporting_agent]to complete market size reports",
         code_execution_config={"last_n_messages": 2, "work_dir": "groupchat","use_docker":"python:3"},
         human_input_mode="NEVER",
     )
-    coder = autogen.AssistantAgent(
-        name="Coder",
+    Market_research_agent = autogen.AssistantAgent(
+        name="Market_research_agent",
+        system_message="You will collect data such as industry reports, market research, and expert opinions. You will strive to gather accurate and complete data according to instructions. Your expertise and experience include market research and data analysis.",
+
         llm_config=llm_config,
     )
-    code_assistant = autogen.AssistantAgent(
-        name="Code_assistant",
-        system_message="You will analyze code, test the code, and provide recomendations",
+    Industry_analysis_agent = autogen.AssistantAgent(
+        name="Industry_analysis_agent",
+        system_message="You will analyze data such as market historical data, market trends, and competitive landscape. You will identify key factors of market size based on this data and provide forecasts. Your expertise and experience include industry analysis and data analysis.",
         llm_config=code_llm_config,
     )
-    pm = autogen.AssistantAgent(
-        name="product_manager",
-        system_message="You will help break down the initial idea into a well scoped requirement for the coder; Do not involve in future conversations or error fixing",
+    Competition_analysis_agent = autogen.AssistantAgent(
+        name="Competition_analysis_agent",
+        system_message="You will analyze data such as competitors' market share, sales volume, and growth rates. You will understand the major competitors in the market and analyze their strengths and weaknesses. Your expertise and experience include competitive analysis and data analysis.",
+        llm_config=pm_llm_config,
+    )
+    Customer_needs_analysis_agent = autogen.AssistantAgent(
+        name="Customer_needs_analysis_agent",
+        system_message="You will analyze data such as customer needs, behavior, and preferences. You will study the demand for AI technology among customers and understand the needs of different industries and businesses of various sizes. Your expertise and experience include customer needs analysis and data analysis",
+        llm_config=pm_llm_config,
+    )
+    Macroeconomic_analysis_agent = autogen.AssistantAgent(
+        name="Macroeconomic_analysis_agent",
+        system_message="You will analyze data such as economic growth, industry investment, and government policies. You will consider how factors like economic growth, industry investment, and government policies affect the AI market. Your expertise and experience include macroeconomic analysis and data analysis",
+        llm_config=pm_llm_config,
+    )
+    Technology_analysis_agent = autogen.AssistantAgent(
+        name="Technology_analysis_agent",
+        system_message="You will analyze data related to technological advancements and the emergence of new technologies. You will assess how technological progress drives market growth and how the advent of new technologies impacts the existing market structure. Your expertise and experience include technology analysis and data analysis.",
+        llm_config=pm_llm_config,
+    )
+    Data_analysis_agent = autogen.AssistantAgent(
+        name="Data_analysis_agent",
+        system_message="You will analyze various types of data, including market data, competitive data, customer data, macroeconomic data, and technological data. Based on the collected data and analysis results, you will develop a market size forecasting model. Your expertise and experience include data analysis, machine learning, and statistics.",
+        llm_config=pm_llm_config,
+    )
+    Market_size_forecasting_agent = autogen.AssistantAgent(
+        name="Market_size_forecasting_agent",
+        system_message="You will utilize models such as time series analysis, regression analysis, and machine learning to build a market size forecast model. Based on collected data and analysis results, you will select the appropriate model and perform parameter estimation. Your expertise and experience are in data analysis, machine learning, and statistics. Specifically, you will follow these steps to build a market size forecasting model:",
+        llm_config=pm_llm_config,
+    )
+    Market_size_simulation_agent = autogen.AssistantAgent(
+        name="Market_size_simulation_agent",
+        system_message="You will conduct simulations for market size forecasting models using software tools such as Excel, R language, and Python. You will perform simulation calculations based on the model's parameters and the scenario being simulated. Your expertise and experience are in data analysis, machine learning, and statistics.",
+        llm_config=pm_llm_config,
+    )
+    Market_size_reporting_agent = autogen.AssistantAgent(
+        name="Market_size_reporting_agent",
+        system_message="You will generate market size reports that may include market size estimation reports with estimated values, growth trends, and driving factors; market competition analysis reports with competitor analysis and competitive landscape; and market opportunity analysis reports with identification of market opportunities and market potential. You will endeavor to produce accurate and useful reports according to your instructions. Your expertise and experience include report writing, report design, and report production.",
         llm_config=pm_llm_config,
     )
 
     # Create groupchat
     groupchat = autogen.GroupChat(
-        agents=[user_proxy, coder, code_assistant, pm], messages=[])
+        agents=[user_proxy, Market_research_agent, Industry_analysis_agent, Competition_analysis_agent,Customer_needs_analysis_agent,Macroeconomic_analysis_agent,Technology_analysis_agent,Data_analysis_agent,Market_size_forecasting_agent,Market_size_simulation_agent,Market_size_reporting_agent], messages=[])
     manager = autogen.GroupChatManager(groupchat=groupchat, llm_config=llm_config)
 
 
